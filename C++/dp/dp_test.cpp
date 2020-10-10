617bf5a6abbed9f4ce9c60edf03c7f94f358403f
tangxiaobaiying <2480045390@qq.com>
2020-10-10 20:52:13 +0800

    背包问题_二维

--- C++/dp/dp_test.cpp
+++ C++/dp/dp_test.cpp
@@ -5,14 +5,13 @@ int dp[6][7]={0};
 int weight[5]={ 3, 2, 1, 4, 5};
 int value[5]= {25,20,15,40,50};
 int main(){
-    for(int i=0;i<=4;i++){
-        for(int j=0;j<=5;j++)
+    for(int i=0;i<=4;i++)
+        for(int j=0;j<=6;j++)
            { 
-            if(i==0||j==0)
-                dp[i][j]=0;
+            if(j<weight[i])
+                dp[i][j]=dp[i-1][j];
             else
                 dp[i][j]=max(dp[i-1][j],dp[i-1][j-weight[i]]+value[i]);
            }
-    }
-    cout<<dp[4][5];
+    cout<<dp[4][6]<<endl;
 }
