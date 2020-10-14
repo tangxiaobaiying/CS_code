fd725aa722bf3509565862c6fe542a374ff40dc1
tangxiaobaiying <2480045390@qq.com>
2020-10-15 02:13:26 +0800

    背包问题

--- C++/dp.cpp
+++ C++/dp.cpp
@@ -1,20 +1,32 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int dp[6][7];
-int weight[5]={3,2,1,4,5};
-int value[5]={25,20,15,40,50};
+// int dp[6][7];
+// int weight[5]={3,2,1,4,5};
+// int value[5]={25,20,15,40,50};
+int lengthOfLIS(vector<int>& );
 int main(){
-    int temp;
-    for(int i=1;i<=5;i++){
-        temp=6-weight[i];
-        if(temp>=0){
-            dp[i][6]=max(dp[i-1][6-weight[i]]+value[i],dp[i-1][6]);
-        }
-        else
-        {
-            dp[i][6]=dp[i-1][6];
-        }
-        cout<<i<<"------"<<dp[i][6]<<endl;
-    }
-    cout<<dp[5][6]<<endl;
+    vector<int>nums={4,10,4,3,8,9};
+    int n=lengthOfLIS(nums);
+    cout<<n<<"--------"<<endl;
+}
+int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int j=0;j<n;j++)
+            for(int i=j;i>=0;i--)
+            {
+                if(i==j){
+                    dp[i][j]=1;
+                    continue;}
+                if(nums[i+1]>nums[i]&&nums[j]<nums[j-1])
+                    dp[i][j]=max(dp[i+1][j]+1,dp[i][j-1]);
+                else if(nums[i+1]<nums[i]&&nums[j]>nums[j-1])
+                    dp[i][j]=max(dp[i+1][j],dp[i][j-1]+1);
+                else if(nums[i+1]>nums[i]&&nums[j]>nums[j-1])
+                    dp[i][j]=max(dp[i+1][j]+1,dp[i][j-1]+1);
+                else if(nums[i+1]<=nums[i]&&nums[j]<=nums[j-1])
+                    dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
+            }
+        return dp[0][n-1];
 }
